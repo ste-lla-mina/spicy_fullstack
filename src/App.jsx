abe480fbb2b54048a39e7d7e5c9c4bc12c5722a1
Stella <nsmizero10@gmail.com>
2026-06-01 08:00:19 +0200

    files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar'; 
 import Hero from './components/Hero';
 import About from './components/About';
+import WhyUs from './components/WhyUs';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <WhyUs/>
     </div>
   );
 }
