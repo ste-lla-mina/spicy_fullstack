2be3a6592aa18904cf840c7ad964c0c96626f85a
Stella <nsmizero10@gmail.com>
2026-06-02 13:57:06 +0200

    owner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar'; 
 import Hero from './components/Hero';
 import About from './components/About';
 import WhyUs from './components/WhyUs';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
 
 function App() {
+  const [view, setView] = useState('landing'); 
+
+  if (view === 'signup') {
+    return <SignUp />;
+  }
+
   return (
     <div className="w-full min-h-screen bg-[#1e1e1e] text-white overflow-x-hidden">
-      <Navbar />
-      <Hero />
+      <Navbar onNavigate={setView} />
+      <Hero onNavigate={setView} />
       <About />
-      <WhyUs/>
+      <WhyUs />
+      <Footer />
     </div>
   );
 }
