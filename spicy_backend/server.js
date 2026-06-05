ba80ca1e84a15707be609ee8ce58b0339f56510c
Stella <nsmizero10@gmail.com>
2026-06-05 08:18:27 +0200

    Server created

---
+++ spicy_backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());         
+app.use(express.json());  
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Restaurant API is running smoothly!' });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🔥 Server ignition complete. Hosting on port http://localhost:${PORT}`);
+});
